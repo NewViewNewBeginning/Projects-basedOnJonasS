f077e672cd21e0c232e21ed4e834cd8f79676809
Michal Krawczyk <michalkrawczyk1@gmail.com>
2022-04-29 17:35:44 +0100

    first funcionality for main functions

--- Guess/app.js
+++ Guess/app.js
@@ -1,1 +1,37 @@
-'use strict'
+"use strict";
+
+let msg = document.querySelector(".message");
+const btnCheck = document.querySelector(".check");
+const btnAgain = document.querySelector(".again");
+let numBox = document.querySelector(".number");
+let score = document.querySelector(".score");
+const highscore = document.querySelector(".higscore");
+
+
+const secretNum = Math.floor(Math.random() * 20) + 1;
+numBox.textContent = secretNum
+
+
+function again() {
+	msg.textContent = "Start guessing...";
+	score.textContent = "20";
+	guessInput.value = "";
+	number.textContent = "?";
+}
+function check() {
+	const guessIn = Number(document.querySelector(".guess").value);
+
+	if (!guessIn) {
+       
+		msg.textContent = "💥 No number 💥";
+	} else if (guessIn === secretNum){
+        msg.textContent = '🌟🌟🌟 Correct Number!!!'
+    }else if(guessIn>secretNum){
+        msg.textContent = 'Your number is too big'
+    }else{
+        msg.textContent = "Your number is too small"
+    }
+}
+
+btnAgain.addEventListener("click", again);
+btnCheck.addEventListener("click", check);
--- Guess/readme.md
+++ Guess/readme.md
@@ -1,1 +1,3 @@
+GUESS MY NUMBER 
+
 
