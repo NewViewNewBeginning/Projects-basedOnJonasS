5c176e42eadec9f6540e3d80a1185f78d4eb7ed3
Michal Krawczyk <michalkrawczyk1@gmail.com>
2022-04-29 19:07:07 +0100

    decreasing of score when wrong but not stopping when hit null

--- Guess/app.js
+++ Guess/app.js
@@ -1,37 +1,41 @@
 "use strict";
-
+const body = document.querySelector("body");
 let msg = document.querySelector(".message");
 const btnCheck = document.querySelector(".check");
 const btnAgain = document.querySelector(".again");
 let numBox = document.querySelector(".number");
-let score = document.querySelector(".score");
+let score = parseFloat(document.querySelector(".score"));
 const highscore = document.querySelector(".higscore");
 
-
 const secretNum = Math.floor(Math.random() * 20) + 1;
-numBox.textContent = secretNum
+console.log(secretNum);
+
 
 
 function again() {
 	msg.textContent = "Start guessing...";
 	score.textContent = "20";
-	guessInput.value = "";
-	number.textContent = "?";
+	numBox.textContent = "?";
 }
 function check() {
-	const guessIn = Number(document.querySelector(".guess").value);
-
+	let guessIn = Number(document.querySelector(".guess").value);
+    
 	if (!guessIn) {
-       
 		msg.textContent = "💥 No number 💥";
-	} else if (guessIn === secretNum){
-        msg.textContent = '🌟🌟🌟 Correct Number!!!'
-    }else if(guessIn>secretNum){
-        msg.textContent = 'Your number is too big'
-    }else{
-        msg.textContent = "Your number is too small"
-    }
-}
+	} else if (guessIn === secretNum) {
+		msg.textContent = "🌟🌟🌟 Correct Number!!!";
+		numBox.textContent = secretNum;
+		body.style.background = "#60b347";
+	} else if (guessIn > secretNum) {
+		msg.textContent = "Your number is too big";
+      
+	} else {
+		msg.textContent = "Your number is too small";
+	}}
+    
+
+
+
 
 btnAgain.addEventListener("click", again);
 btnCheck.addEventListener("click", check);
