c1a9f6eba600aa7a0e8506b9733db400e32f081b
Michal Krawczyk <michalkrawczyk1@gmail.com>
2022-05-04 14:53:42 +0100

    basic funcionality for displaying score and updating msg when lost

--- Guess/app.js
+++ Guess/app.js
@@ -4,13 +4,13 @@ let msg = document.querySelector(".message");
 const btnCheck = document.querySelector(".check");
 const btnAgain = document.querySelector(".again");
 let numBox = document.querySelector(".number");
-let score = parseFloat(document.querySelector(".score"));
+let score = document.querySelector(".score");
 const highscore = document.querySelector(".higscore");
 
 const secretNum = Math.floor(Math.random() * 20) + 1;
 console.log(secretNum);
 
-
+let startingScore = 20;
 
 function again() {
 	msg.textContent = "Start guessing...";
@@ -19,7 +19,7 @@ function again() {
 }
 function check() {
 	let guessIn = Number(document.querySelector(".guess").value);
-    
+
 	if (!guessIn) {
 		msg.textContent = "💥 No number 💥";
 	} else if (guessIn === secretNum) {
@@ -27,15 +27,25 @@ function check() {
 		numBox.textContent = secretNum;
 		body.style.background = "#60b347";
 	} else if (guessIn > secretNum) {
-		msg.textContent = "Your number is too big";
-      
-	} else {
-		msg.textContent = "Your number is too small";
-	}}
-    
-
-
-
+		if (startingScore > 1) {
+			msg.textContent = "Your number is too big";
+			startingScore--;
+			score.textContent = startingScore;
+		} else {
+			msg.textContent = "You lost the game 💀💀💀";
+			score.textContent = 0;
+		}
+	} else if (guessIn < secretNum) {
+		if (startingScore > 1) {
+			msg.textContent = "Your number is too small";
+			startingScore--;
+			score.textContent = startingScore;
+		} else {
+			msg.textContent = "You lost the game 💀💀💀";
+			score.textContent = 0;
+		}
+	}
+}
 
 btnAgain.addEventListener("click", again);
 btnCheck.addEventListener("click", check);
