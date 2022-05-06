0ef8d8f2d1bc4bc5c35d6e6835f35094c1257c47
Michal Krawczyk <michalkrawczyk1@gmail.com>
2022-05-06 15:11:18 +0100

    Update app.js

--- modalWindow/app.js
+++ modalWindow/app.js
@@ -4,23 +4,25 @@ const overlay = document.querySelector(".overlay");
 const btnsOpen = document.querySelectorAll(".show-modal");
 const closeModalBtn = document.querySelector(".close-modal");
 
-const closeModal = (e) => { 
-    overlay.classList.add("hidden");
+const closeModal = e => {
+	overlay.classList.add("hidden");
 	modal.classList.add("hidden");
- }
+};
 
+// Opening modal with click on any from 3 bns
 btnsOpen.forEach(function (btn) {
 	btn.addEventListener("click", function () {
 		overlay.classList.toggle("hidden");
 		modal.classList.toggle("hidden");
 	});
 });
+//Closing mod
+overlay.addEventListener('click',closeModal)
 closeModalBtn.addEventListener("click", closeModal);
 
-
-document.onkeydown = function(e){
-    e = e || window.event;
-    if(e.keyCode == 27){
-        closeModal()
-    }
-}
+document.onkeydown = function (e) {
+	e = e || window.event;
+	if (e.keyCode == 27) {
+		closeModal();
+	}
+};
