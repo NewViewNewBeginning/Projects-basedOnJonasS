8e6b937ba28166a5e68ce693c63c1152b86f8cd2
Michal Krawczyk <michalkrawczyk1@gmail.com>
2022-05-06 14:56:26 +0100

    Update app.js

--- modalWindow/app.js
+++ modalWindow/app.js
@@ -4,14 +4,23 @@ const overlay = document.querySelector(".overlay");
 const btnsOpen = document.querySelectorAll(".show-modal");
 const closeModalBtn = document.querySelector(".close-modal");
 
+const closeModal = (e) => { 
+    overlay.classList.add("hidden");
+	modal.classList.add("hidden");
+ }
+
 btnsOpen.forEach(function (btn) {
 	btn.addEventListener("click", function () {
-			overlay.classList.toggle("hidden");
-			modal.classList.toggle("hidden");
-		
+		overlay.classList.toggle("hidden");
+		modal.classList.toggle("hidden");
 	});
 });
-closeModalBtn.addEventListener("click", function () {
-	overlay.classList.add("hidden");
-	modal.classList.add("hidden");
-});
+closeModalBtn.addEventListener("click", closeModal);
+
+
+document.onkeydown = function(e){
+    e = e || window.event;
+    if(e.keyCode == 27){
+        closeModal()
+    }
+}
